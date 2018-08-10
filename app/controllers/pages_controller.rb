5a7452e68df2c0a0c994779663971edcb1c82e36
Philipp Stoll <Philipp.Stoll97@gmx.de>
2018-08-10 22:11:38 +0200

    hoffnung
    änderung1

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -8,4 +8,34 @@ def agb_page
 
 end
 
+def test_page
+
+end
+
+def login_page
+
+end
+
+def impfpass_page
+
+end
+
+def wikiMain_page
+
+end
+
+def wikiGelbfieber_page
+
+end
+
+  def sozialeProjekte_page
+
+  end
+
+
+def pdf
+  pdf_filename = File.join(Rails.root, "app/assets/downloads/VeröffentlichungsAntrag.pdf")
+  send_file(pdf_filename, :filename => "VeröffentlichungsAntrag.pdf", :disposition => 'inline', :type => "application/pdf")
+end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,14 @@ Rails.application.routes.draw do
   devise_for :users
 
   root 'pages#welcome_page'
+  get '/welcome',to:'pages#welcome_page'
+  get '/main',to:'pages#test_page'
   get '/agb', to:'pages#agb_page'
+  get '/login', to:'pages#login_page'
+  get '/impfpass', to:'pages#impfpass_page'
+  get '/wikiMain', to:'pages#wikiMain_page'
+  get '/gelbfieber', to:'pages#wikiGelbfieber_page'
+  get '/socialProjects', to: 'pages#sozialeProjekte_page'
+  get '/VeröffentlichungsAntrag', to: 'pages#pdf' , as: :ProjectFormular
 
 end
