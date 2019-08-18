278831e199cb95ab5136eb17156e862f3ac386f3
littleclock <littleclock@gmx.de>
2019-08-19 00:20:23 +0200

    Update map_entries_controller.rb
    search method should now work as expected, just the values need to be adapted

--- app/controllers/map_entries_controller.rb
+++ app/controllers/map_entries_controller.rb
@@ -64,18 +64,32 @@ class MapEntriesController < ApplicationController
   end
 
   def search
+    # right now implemented spec as 0 and distance as 1
+    typ = params[:typ]
     search = params[:search]
-    a=[]
-    @map_entries = MapEntry.all
-    @map_entries.each do |e|
-      if e.spec == search.spec
-        a << e
-      end
+    coords = params[:coords]
+    case typ
+    when 0
+      return MapEntry.all.where(spec: search.spec).to_json
+    when 1
+          a=[]
+          @map_entries = MapEntry.all
+          @map_entries.each do |e|
+            point1 = [e.lat, e.lng]
+            point2 = [coords.x, coords.y]
+            dist = calc_distance(point1,point2)
+            if dist <= search.dist
+              a << e
+            end
+          end
+          a.to_json
+    else
+      return ''
     end
-    a.to_json
   end
 
   private
+    COORD_PARSE_REGEX = /(\d+)°(\d+)′(\d+)″([NSEW])/
     # Use callbacks to share common setup or constraints between actions.
     def set_map_entry
       @map_entry = MapEntry.find(params[:id])
@@ -84,5 +98,52 @@ class MapEntriesController < ApplicationController
     # Never trust parameters from the scary internet, only allow the white list through.
     def map_entry_params
       params.require(:map_entry).permit(:name, :plz, :location, :street, :phone, :spec, :lat, :lng)
+    end
+
+    def calc_distance(point1, point2)
+      r = 6371
+      x1, y1 = coords_to_rads(point1)
+      x2, y2 = coords_to_rads(point2)
+      dx = x1 - x2
+      dy = y1 - y2
+
+      a = Math.sin(dx/2)**2 + Math.cos(x1) * Math.cos(x2) * Math.sin(dy/2)**2
+      c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a))
+      r * c
+    end
+
+    def coords_to_rads (point)
+      if point.length == 2
+        if point[0].is_a? Numeric
+          x = point[0]
+        elsif point[0].is_a? String # cd cardinal direction, d degree, m minutes, s seconds
+          d, m, s, cd = point[0].scan(COORD_PARSE_REGEX)
+          x = radians(int(d) + int(m)/60 + int(s)/3600)
+          if cd=='S' || cd=='W'
+            x*=-1
+          end
+        else
+          x = 0
+        end
+
+        if point[1].is_a? Numeric
+          y = point[1]
+        elsif point[1].is_a? String # cd cardinal direction, d degree, m minutes, s seconds
+          d, m, s, cd = point[1].scan(COORD_PARSE_REGEX)
+          y = radians(int(d) + int(m)/60 + int(s)/3600)
+          if cd=='S' || cd=='W'
+            y*=-1
+          end
+        else
+          y = 0
+        end
+        return x,y
+      else
+        return 0,0
+      end
+    end
+
+    def radians (degrees)
+      degrees * Math::PI / 180
     end
 end
