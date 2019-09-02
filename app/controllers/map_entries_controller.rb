d7ef99157a36daf513f07f18dafec8b95d360513
littleclock <littleclock@gmx.de>
2019-09-02 16:41:01 +0200

    Update map_entries_controller.rb
    enhanced Performance of search method

--- app/controllers/map_entries_controller.rb
+++ app/controllers/map_entries_controller.rb
@@ -73,11 +73,11 @@ class MapEntriesController < ApplicationController
       return MapEntry.all.where(spec: search.spec).to_json
     when 1
           a=[]
+          xp, yp = coords_to_rads([coords.x, coords.y])
           @map_entries = MapEntry.all
           @map_entries.each do |e|
-            point1 = [e.lat, e.lng]
-            point2 = [coords.x, coords.y]
-            dist = calc_distance(point1,point2)
+            xt, yt = coords_to_rads( [e.lat, e.lng] )
+            dist = calc_distance(xt, yt, xp, yp)
             if dist <= search.dist
               a << e
             end
@@ -100,10 +100,8 @@ class MapEntriesController < ApplicationController
       params.require(:map_entry).permit(:name, :plz, :location, :street, :phone, :spec, :lat, :lng)
     end
 
-    def calc_distance(point1, point2)
+    def calc_distance(x1, y1, x2, y2)
       r = 6371
-      x1, y1 = coords_to_rads(point1)
-      x2, y2 = coords_to_rads(point2)
       dx = x1 - x2
       dy = y1 - y2
 
@@ -113,34 +111,23 @@ class MapEntriesController < ApplicationController
     end
 
     def coords_to_rads (point)
+      pc =[0,0]
       if point.length == 2
-        if point[0].is_a? Numeric
-          x = point[0]
-        elsif point[0].is_a? String # cd cardinal direction, d degree, m minutes, s seconds
-          d, m, s, cd = point[0].scan(COORD_PARSE_REGEX)
-          x = radians(int(d) + int(m)/60 + int(s)/3600)
-          if cd=='S' || cd=='W'
-            x*=-1
+          point.to_h.each do |index, value|
+            if value.is_a? Numeric
+              pc[index] = value
+            elsif value.is_a? String # cd cardinal direction, d degree, m minutes, s seconds
+              d, m, s, cd = value.scan(COORD_PARSE_REGEX)
+              pc[index] = radians(int(d) + int(m)/60 + int(s)/3600)
+              if cd=='S' || cd=='W'
+                pc[index]*=-1
+              end
+            else
+              pc[index] = 0
+            end
           end
-        else
-          x = 0
-        end
-
-        if point[1].is_a? Numeric
-          y = point[1]
-        elsif point[1].is_a? String # cd cardinal direction, d degree, m minutes, s seconds
-          d, m, s, cd = point[1].scan(COORD_PARSE_REGEX)
-          y = radians(int(d) + int(m)/60 + int(s)/3600)
-          if cd=='S' || cd=='W'
-            y*=-1
-          end
-        else
-          y = 0
-        end
-        return x,y
-      else
-        return 0,0
       end
+      pc
     end
 
     def radians (degrees)
