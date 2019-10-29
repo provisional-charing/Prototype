681c85fa5591535cadf91740c7e82ee39b225684
littleclock <littleclock@gmx.de>
2019-10-29 21:09:34 +0100

    update for provinces

--- app/controllers/provinces_controller.rb
+++ app/controllers/provinces_controller.rb
@@ -13,4 +13,19 @@ class ProvincesController < ApplicationController
     end
   end
 
+  # ids deefine which should be returned
+  def matching_indices(max)
+    ids = RegionLethality.get_regions(max)
+    @provinces = Province.where(id: ids)
+
+    respond_to do |format|
+      format.json do
+        feature_collection = Province.to_feature_collection @provinces
+        render json: RGeo::GeoJSON.encode(feature_collection)
+      end
+
+      format.html
+    end
+  end
+
 end
--- app/models/region_lethality.rb
+++ app/models/region_lethality.rb
@@ -1,3 +1,6 @@
 class RegionLethality < ApplicationRecord
 
+  def get_regions(max)
+    RegionLethality.find_each(:lethality <= max, :lethality>0 ).pluck(:province_id)
+  end
 end
